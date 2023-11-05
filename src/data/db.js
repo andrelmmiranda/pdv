98da3ff7598960d68f7217494c7d641973b67099
thiagopedro99 <thiago.gefa@gmail.com>
2023-11-05 10:47:20 -0300

    feat: Criado o endpoint de cadastrar usuário

--- src/data/db.js
+++ src/data/db.js
@@ -3,9 +3,23 @@ const pool = require('../config/connection')
 
 module.exports = {
 
+    async createUser(nome, email, senha) {
+        const query = "INSERT INTO usuarios (nome, email, senha) VALUES ($1, $2, $3) RETURNING id, nome, email"
+        const values = [nome, email, senha]
+        const usuario = await pool.query(query, values)
+        return usuario
+    },
+
+    async existeEmailCadastrado(email) {
+        const query = "SELECT * FROM usuarios WHERE email = $1"
+        const values = [email]
+        const usuario = await pool.query(query, values)
+        return usuario.rowCount != 0
+    },
+
     async getCategories() {
         const query = "SELECT * FROM categorias"
         const categorias = await pool.query(query)
         return categorias.rows
-    },
+    }
 }
