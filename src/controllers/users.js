eb8071147e8224446a7083c821b230677b92c11c
Rafael <francorfs@icloud.com>
2023-11-06 16:43:53 -0300

    Criado login de usuario

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,6 @@
-const pool = require('../data/db')
+const pool = require('../config/connection')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const { validarCampos } = require('../utils/validarcampos')
 require('dotenv').config()
 
@@ -7,10 +8,6 @@ require('dotenv').config()
 const casdastrarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body
 
-
-    if (!validarCampos(nome, email, senha)) {
-        return res.status(400).json({ mensagem: 'Todos os campos devem ser preenchidos' })
-    }
 
     try {
 
@@ -29,22 +26,41 @@ const casdastrarUsuario = async (req, res) => {
         return res.status(500).json({ mensagem: 'Erro inesperado do servidor' })
     }
 }
+const login = async (req, res) => {
+    const { email, senha } = req.body
 
-module.exports = {
-    casdastrarUsuario
+    if (!validarCampos(email, senha)) {
+        return res.status(400).json({ mensagem: 'Email ou senha são obrigatórios' })
+    }
+
+    try {
+        const usuario = await pool.query('select * from usuarios where email = $1', [email])
+
+        if (usuario.rowCount < 1) {
+            return res.status(404).json({ mensagem: 'Email ou senha inválidos' })
+        }
+        const senhaValida = await bcrypt.compare(senha, usuario.rows[0].senha)
+
+        if (!senhaValida) {
+            return res.status(404).json({ mensagem: 'Email ou senha inválidos' })
+        }
+        const token = jwt.sign({ id: usuario.rows[0].id }, process.env.SENHA, { expiresIn: '8h' })
+
+        const { senha: _, ...usuarioLogado } = usuario.rows[0]
+
+        return res.json({ usuario: usuarioLogado, token })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ mensagem: 'Erro inesperado do servidor' })
+    }
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
+module.exports = {
+    casdastrarUsuario,
+    login
+}
 
 
 
