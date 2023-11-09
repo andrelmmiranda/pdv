47bd1b1c32a1db4ebf422583cd6f2dc238861294
Rafael <francorfs@icloud.com>
2023-11-08 22:30:47 -0300

    Atualizaçao nas configuraçoes da conexao

--- src/config/connection.js
+++ src/config/connection.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'pdv'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 module.exports = pool;
