20767617cc1175753e0f927fc954bc3ee82d1d0c
thiagopedro99 <thiago.gefa@gmail.com>
2023-11-09 15:18:29 -0300

    Feat: Use cors in api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const rotas = require('./routers/routers');
 const app  = express()
+const cors = require('cors');
 
-
+app.use(cors());
 app.use(express.json());
 app.use(rotas)
 
