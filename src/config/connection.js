6d7c4d58078ffce5a58effedc20d5b1db43e91ad
thiagopedro99 <thiago.gefa@gmail.com>
2023-11-08 23:22:14 -0300

    Feat: Deploy postgrs connection

--- src/config/connection.js
+++ src/config/connection.js
@@ -2,11 +2,7 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'pdv'
+    connectionString: process.env.POSTGRES_URL
 })
-
 module.exports = pool;
+
