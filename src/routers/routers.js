a3a400d1e4bc3d3791ab648389850a123350bbbd
Rafael <francorfs@icloud.com>
2023-11-14 01:13:06 -0300

    Adicionado rota editar produto

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { listarCategorias } = require('../controllers/categorias');
 const autenticarUsuario = require('../utils/authentication');
 const { casdastrarUsuario, login, editarUsuario, detalharUsuario } = require('../controllers/users');
-const { cadastrarProduto, listarProdutos } = require('../controllers/products');
+const { cadastrarProduto, listarProdutos, editarProduto } = require('../controllers/products');
 
 const rotas = express.Router();
 
@@ -17,6 +17,7 @@ rotas.get('/usuario', detalharUsuario)
 rotas.put('/usuario', editarUsuario)
 
 rotas.post('/produto', cadastrarProduto)
+rotas.put('/produto/:id', editarProduto)
 rotas.get('/produto', listarProdutos)
 
 module.exports = rotas
