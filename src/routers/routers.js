c7ce01544dd50c9b9a947487accaa8da99f20bd7
Rafael <francorfs@icloud.com>
2023-11-14 00:13:32 -0300

    Adicionado rota de listar produto

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { listarCategorias } = require('../controllers/categorias');
 const autenticarUsuario = require('../utils/authentication');
 const { casdastrarUsuario, login, editarUsuario, detalharUsuario } = require('../controllers/users');
-const { cadastrarProduto } = require('../controllers/products');
+const { cadastrarProduto, listarProdutos } = require('../controllers/products');
 
 const rotas = express.Router();
 
@@ -16,6 +16,7 @@ rotas.use(autenticarUsuario)
 rotas.get('/usuario', detalharUsuario)
 rotas.put('/usuario', editarUsuario)
 
-rotas.post('produto', cadastrarProduto)
+rotas.post('/produto', cadastrarProduto)
+rotas.get('/produto', listarProdutos)
 
 module.exports = rotas
