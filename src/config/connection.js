a204f40043e3a3678430547aa664032f1ba8c067
thiagopedro99 <thiago.gefa@gmail.com>
2023-11-20 13:11:56 -0300

    Connection database config

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,13 +1,20 @@
-const { Pool } = require('pg')
-require('dotenv').config()
+const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+let pool; // Declaração da variável pool fora do bloco condicional
+
+if (process.env.NODE_ENV === 'teste') {
+    pool = new Pool({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+} else {
+    pool = new Pool({
+        connectionString: process.env.POSTGRES_URL + "?sslmode=require"
+    });
+}
 
 module.exports = pool;
-
